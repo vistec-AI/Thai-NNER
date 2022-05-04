1ed69f6ab330f202bb3019ba19815a022ef59d03
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-05-05 01:59:18 +0700

    Update __init__.py

--- thai_nner/__init__.py
+++ thai_nner/__init__.py
@@ -11,19 +11,18 @@ thai_nner_path = os.path.dirname(thai_nner.__file__)
 
 
 class NNER:
-    def __init__(self, path_model: str, num_classes: int=417, num_layers: int=8, max_sent_length: int=512, path_lm: str="airesearch/wangchanberta-base-att-spm-uncased", path_data: str=os.path.join(thai_nner_path,"config.json"), boundary_type: str="BIESO", device="cpu") -> None:
+    def __init__(self, path_model: str, num_classes: int=417, num_layers: int=8, max_sent_length: int=512, path_lm: str="airesearch/wangchanberta-base-att-spm-uncased", path_data: str=os.path.join(thai_nner_path,"config.json"), boundary_type: str="BIESO") -> None:
         self.path_model = path_model
         self.max_sent_length = max_sent_length
         self.path_lm = path_lm
         self.model = NNEModel(num_classes=num_classes,num_layers=num_layers,path_lm=path_lm)
-        self.model.to(torch.device(device))
         self.state_dict_new = OrderedDict()
         try:
-            self.state_dict = torch.load(self.path_model, map_location=torch.device(device))
+            self.state_dict = torch.load(self.path_model)
         except NotImplementedError:
             temp = pathlib.PosixPath
             pathlib.PosixPath = pathlib.WindowsPath
-            self.state_dict = torch.load(self.path_model, map_location=torch.device(device))
+            self.state_dict = torch.load(self.path_model)
         self.model.load_state_dict(self.state_dict)
         self.PAD = '<pad>'
         self.data = Data(path_data=path_data, boundary_type=boundary_type)
